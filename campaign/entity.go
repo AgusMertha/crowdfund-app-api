e60218ea22597b576f141b39e62ea99e93ee5d4e
AgusMertha <agusmertha.am@gmail.com>
2022-04-25 14:04:42 +0800

    campaign repository

--- campaign/entity.go
+++ campaign/entity.go
@@ -1,1 +1,28 @@
 package campaign
+
+import "time"
+
+type Campaign struct {
+	Id               int             `json:"id"`
+	UserId           int             `json:"user_id"`
+	Name             string          `json:"name"`
+	ShortDescription string          `json:"short_description"`
+	Description      string          `json:"description"`
+	GoalAmount       int             `json:"goal_amount"`
+	CurrnetAMount    int             `json:"currnet_amount"`
+	Perks            string          `json:"perks"`
+	BackerCount      string          `json:"backer_count"`
+	Slug             string          `json:"slug"`
+	CreatedAt        time.Time       `json:"created_at"`
+	UpdatedAt        time.Time       `json:"updated_at"`
+	CampaignImages   []CampaignImage `json:"campaign_images"`
+}
+
+type CampaignImage struct {
+	Id         int       `json:"id"`
+	CampaignId int       `json:"campaign_id"`
+	FileName   string    `json:"file_name"`
+	IsPrimary  bool      `json:"is_primary"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,38 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type CampaignRepository interface {
+	FindAll() ([]Campaign, error)
+	FindByUserId(userId int) ([]Campaign, error)
+}
+
+type CampaignRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewCampaignRepository(db *gorm.DB) *CampaignRepositoryImpl {
+	return &CampaignRepositoryImpl{db}
+}
+
+func (c *CampaignRepositoryImpl) FindAll() ([]Campaign, error) {
+	var campaigns []Campaign
+	err := c.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
+
+func (c *CampaignRepositoryImpl) FindByUserId(userId int) ([]Campaign, error) {
+	var campaigns []Campaign
+	err := c.db.Where("user_id = ?", userId).Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
